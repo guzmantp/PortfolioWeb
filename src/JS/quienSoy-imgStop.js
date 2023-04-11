51f376c67d2948fadfd065b45a129353165cbdb1
guzman_tp <guzman_toledano@hotmail.com>
2023-04-11 22:23:10 +0200

    UPDATE: End day

--- src/JS/quienSoy-imgStop.js
+++ src/JS/quienSoy-imgStop.js
@@ -1,12 +1,18 @@
 window.addEventListener("scroll", function(){
-    var table = this.document.getElementById("table-container").getBoundingClientRect().top;
-    var scrollTop = this.window.pageYOffset || this.document.documentElement.scrollTop;
-    var foto = document.getElementById("fotoMia-Container");
+   var leftCont = this.document.getElementById("left-container");
+   var leftContHeight = leftCont.offsetHeight;
+   var foto = this.document.getElementById("fotoMia-Container");
+   var fotodistance = foto.offsetTop;
 
-    if (scrollTop + 100 >= table) {
-        foto.style.position = "absolute";
-        foto.style.bottom = "0";
-    } else {
-        foto.style.position = "fixed";
-    }
+   console.log("-------------------------");
+   console.log("leftContHeight: " + leftContHeight);
+   console.log("fotoDistance " + fotodistance);
+
+   if(leftContHeight >= fotodistance){
+    leftCont.style.position = "absolute";
+   }else{
+    leftCont.style.position = "fixed";
+   };
+    
+    
 });
