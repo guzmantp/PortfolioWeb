4a6eb100df121ec5319c9a3cce74877bca48318e
Guzman <guzman_toledano@hotmail.com>
2023-04-12 00:01:54 +0200

    BUG FIXED: Sobre mi page scroll effect fixed

--- src/JS/quienSoy-imgStop.js
+++ src/JS/quienSoy-imgStop.js
@@ -1,18 +1,21 @@
-window.addEventListener("scroll", function(){
-   var leftCont = this.document.getElementById("left-container");
-   var leftContHeight = leftCont.offsetHeight;
-   var foto = this.document.getElementById("fotoMia-Container");
-   var fotodistance = foto.offsetTop;
+document.addEventListener("DOMContentLoaded", function () {
+   const fotoMiaContainer = document.getElementById("fotoMia-Container");
+   const leftContainer = document.getElementById("left-container");
+   
+   window.addEventListener("scroll", function () { 
+     const scroll = (window.pageYOffset || document.documentElement.scrollTop);
+      
+    if(scroll + this.window.innerHeight  >= leftContainer.offsetHeight){
+      fotoMiaContainer.style.position = "absolute";
+    }else{
+      fotoMiaContainer.style.position = "fixed";
+    }
+   });
+ });
+ 
 
-   console.log("-------------------------");
-   console.log("leftContHeight: " + leftContHeight);
-   console.log("fotoDistance " + fotodistance);
 
-   if(leftContHeight >= fotodistance){
-    leftCont.style.position = "absolute";
-   }else{
-    leftCont.style.position = "fixed";
-   };
-    
-    
-});
+
+
+
+ 
